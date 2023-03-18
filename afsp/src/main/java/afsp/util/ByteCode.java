18f2a19cabc128ce6da7a4da1277d02f903776ab
Jeroen Rijkse <jeroen@rijk.se>
2023-03-18 10:44:52 +0100

    partially finished

--- afsp/src/main/java/afsp/util/ByteCode.java
+++ afsp/src/main/java/afsp/util/ByteCode.java
@@ -4,7 +4,8 @@ public enum ByteCode {
     SP(0x20),
     CR(0x0D),
     LF(0x0A),
-    COL(0x3A);
+    COL(0x3A),
+    ESC(0x5C);
     public int code;
     ByteCode(int _byte){
         this.code = _byte;
---
+++ afsp/src/main/java/afsp/util/Utils.java
@@ -0,0 +1,2 @@
+package afsp.util;public class Utils {
+}
