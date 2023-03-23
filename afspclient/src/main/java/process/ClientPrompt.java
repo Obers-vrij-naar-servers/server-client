c06b69f2eb2b71cfff05c8365ee4d095f24a8c13
Jeroen Rijkse <jeroen@rijk.se>
2023-03-23 10:50:02 +0100

    rebase

---
+++ afspclient/src/main/java/process/ClientPrompt.java
@@ -0,0 +1,69 @@
+package process;
+
+import config.Configuration;
+
+import java.util.Scanner;
+
+public class ClientPrompt {
+
+    private MenuOption selectedMenuOption;
+    private final RequestHandler requestHandler;
+    private boolean firstPrompt = true;
+
+    public ClientPrompt(Configuration conf) {
+        this.requestHandler = new RequestHandler(conf);
+    }
+
+    public void start() {
+        do {
+            prompt();
+
+            if(selectedMenuOption != null) {
+                //TODO make a real response
+                var response = requestHandler.handle();
+                if(response) {
+                    selectedMenuOption = null;
+                }
+            }
+
+            this.firstPrompt = false;
+
+        } while (selectedMenuOption != MenuOption.EXIT);
+    }
+
+    public void prompt() {
+        MenuOption[] options = MenuOption.values();
+
+        if(this.firstPrompt) {
+            System.out.println("Select an option:");
+        } else {
+            System.out.println("Select another option:");
+        }
+
+        for (int i = 0; i < options.length; i++) {
+            System.out.println((i + 1) + ". " + options[i].getOptionText());
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int selectedOption = 0;
+        boolean validOption = false;
+        while (!validOption) {
+            System.out.print("Enter option number: ");
+
+            if (scanner.hasNextInt()) {
+                selectedOption = scanner.nextInt();
+                if (selectedOption >= 1 && selectedOption <= options.length) {
+                    validOption = true;
+                } else {
+                    System.out.println("Invalid option. Please enter a number between 1 and " + options.length);
+                }
+            } else {
+                System.out.println("Invalid input. Please enter a number.");
+                scanner.next();
+            }
+        }
+
+        this.selectedMenuOption = options[selectedOption - 1];
+        System.out.println("You selected: " + selectedMenuOption.getOptionText());
+    }
+}
