5b75cf1458d6f5acf3b3a1edc763c5624084819a
Jeroen Rijkse <jeroen@rijk.se>
2023-05-14 21:04:39 +0200

    renamed test

--- afsp/src/test/java/AfspRequestParserTest.java
+++ afsp/src/test/java/AfspRequestParserTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
-class TestAfspRequestParser {
+class AfspRequestParserTest {
 
     private InputStreamMocker mocker = new InputStreamMocker();
 
